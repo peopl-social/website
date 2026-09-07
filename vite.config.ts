91e4f18cb865520b5b5f039d3da20c8bf8b53ed9
Jakub Sova <jakubsova@protonmail.com>
2026-09-07 12:22:10 +0000

    bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
   fmt: {},
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
