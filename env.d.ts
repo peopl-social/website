7eba7338e462c71072e68346f513418151a337d7
Rudhransh <244164046+rud-998@users.noreply.github.com>
2026-09-20 23:23:43 +0530

    ci error2

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,13 @@
+import type { ModuleOptions } from "nuxt-i18n-micro";
+
 interface ImportMeta {
   readonly dev: boolean;
+}
+
+declare module "@nuxt/schema" {
+  interface NuxtConfig {
+    i18n?: ModuleOptions;
+  }
 }
 
 declare module "*.vue" {
